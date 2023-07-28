a93156b45075d67e880cf49a1321ca9b19ce6e08
hanqiangqiang <3354378874@qq.com>
2023-07-28 10:01:10 +0800

    提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    title: '我的博客',
+    description: 'XXX',
+    theme: 'reco',
+    themeConfig: {
+        subSidebar: 'auto',
+    },
+    base: '/hanblog/',
+    locales: {
+        '/': {
+          lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        nav: [
+          { text: "首页", link: "/" },
+          {
+            text: "汉堡克的博客",
+            items: [
+              { text: "掘金", link: "https://juejin.cn/user/726107228492253" },
+              { text: "Github", link: "https://github.com/coding-lin" }
+            ]
+          }
+        ],
+        sidebar: [
+            {
+              title: "欢迎学习",
+              path: "/",
+              collapsable: false,  // 是否折叠
+              children: [{ title: "博客简介", path: "/" }],
+            },
+            {
+              title: "基础篇",
+              path: "/handbook/1",
+              collapsable: true,
+              children: [
+                { title: "第一篇", path: "/handbook/1" },
+                { title: "第二篇", path: "/handbook/2" },
+              ]
+            } 
+          ]
+    }
+}
